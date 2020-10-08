a7ce0a61fc4f828703b27f61cea1445be6f9795b
Antolina Gonzalez <antolina487@gmail.com>
2020-10-08 09:23:57 -0300

    intento i+1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,37 +1,31 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { CoursesModule } from './courses/courses.module';
-import { MongooseModule } from '@nestjs/mongoose';
-import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from './config/config.module';
-import { ConfigService } from './config/config.service';
-import { Configuration } from './config/config.keys';
-import { ContentsModule } from './contents/contents.module';
-
-
-
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UsersModule } from "./users/users.module";
+import { CoursesModule } from "./courses/courses.module";
+import { MongooseModule } from "@nestjs/mongoose";
+import { AuthModule } from "./auth/auth.module";
+import { ConfigModule } from "./config/config.module";
+import { ConfigService } from "./config/config.service";
+import { Configuration } from "./config/config.keys";
+import { ContentsModule } from "./contents/contents.module";
 
 @Module({
   controllers: [AppController],
   imports: [
     CoursesModule,
-    MongooseModule.forRoot('mongodb+srv://bmrec:oLM36WuO5ZUKlU9o@cluster0.y2qus.mongodb.net/ticmas?retryWrites=true&w=majority', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    }),
+    MongooseModule.forRoot(
+      "mongodb+srv://bmrec:oLM36WuO5ZUKlU9o@cluster0.y2qus.mongodb.net/ticmas?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+      }
+    ),
     UsersModule,
     AuthModule,
     ConfigModule,
   ],
   providers: [AppService],
 })
-export class AppModule {
-  static port: number | string;
-
-  constructor(private readonly _configService: ConfigService) {
-    AppModule.port = process.env.PORT;
-  }
-}
+export class AppModule {}
